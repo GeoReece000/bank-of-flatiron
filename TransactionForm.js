9bc28e5b0de0c84243d13afef87da2ed26bb56f4
GeoReece000 <geofreytegeret@gmail.com>
2023-04-25 11:29:09 +0300

    TFfile

---
+++ TransactionForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const TransactionForm = ({ onAddTransaction }) => {
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTransaction = {
+      date,
+      description,
+      category,
+      amount: parseFloat(amount),
+    };
+    onAddTransaction(newTransaction);
+    setDate("");
+    setDescription("");
+    setCategory("");
+    setAmount("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Date:
+        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+      </label>
+      <label>
+        Description:
+        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} required />
+      </label>
+      <label>
+        Category:
+        <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} required />
+      </label>
+      <label>
+        Amount:
+        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} required />
+      </label>
+      <button type="submit">Add Transaction</button>
+    </form>
+  );
+};
+
+export default TransactionForm;
